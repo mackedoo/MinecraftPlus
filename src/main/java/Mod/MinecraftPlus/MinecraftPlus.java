923edd0f19de1178d26f213b1c3f56b48d335dfd
Benjamin Cheng <lolzballs.mc@gmail.com>
2014-10-17 23:49:43 -0400

    Tree gen might be working...

--- src/main/java/Mod/MinecraftPlus/MinecraftPlus.java
+++ src/main/java/Mod/MinecraftPlus/MinecraftPlus.java
@@ -14,6 +14,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.util.EnumHelper;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
